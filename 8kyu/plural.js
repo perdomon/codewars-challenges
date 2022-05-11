d1c2a1cddde0d529a509ad6d415c78ea82917c00
Nate Perdomo <101995665+perdomon@users.noreply.github.com>
2022-05-10 20:02:19 -0500

    Create plural.js
    determine if a plural ending should be used on a string

---
+++ 8kyu/plural.js
@@ -0,0 +1,9 @@
+// Plural
+// Take a number and return 'yes' if a plural should be used, 'no' if not
+
+function plural(n) {
+  return(n !== 1)
+  }
+
+// Pay attention to the phrasing of questions. This problem is easiest solved when trying to accomplish the singular side 
+// of the question vs. focusing on plurals and zero.
