e229ccc2fdba22ddc22cfb06769816237ccff937
Nate Perdomo <101995665+perdomon@users.noreply.github.com>
2022-05-11 18:02:36 -0500

    Create stringToArray.js

---
+++ 8kyu/stringToArray.js
@@ -0,0 +1,7 @@
+// 8kyu Convert a string to an array
+// Write a function to split a string and convert it into an array of words.
+
+function stringToArray(string){
+  const array = string.split(' ')
+  return array
+}
