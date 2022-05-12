fdb9417592a02711a842c1a20345ee5a0096ed77
Nate Perdomo <101995665+perdomon@users.noreply.github.com>
2022-05-12 14:30:45 -0500

    Create parseInt.js

---
+++ 8kyu/parseInt.js
@@ -0,0 +1,6 @@
+// 8kyu Parse nice int from char problem
+// Given an integral number, determine if it's a square number:
+
+function getAge(inputString){
+  return (parseInt(inputString.charAt(0)))
+}
