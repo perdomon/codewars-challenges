6b3833a7bacf4a96c6da94ed9abb3089cf367ef2
Nate Perdomo <101995665+perdomon@users.noreply.github.com>
2022-05-12 14:40:30 -0500

    Create booleanToString.js

---
+++ 8kyu/booleanToString.js
@@ -0,0 +1,11 @@
+// 8kyu Convert Boolean to String
+
+// If boolean value is true, return 'true.' If boolean value is false, return 'false.'
+
+function booleanToString(b){
+  if(b===true){
+    return 'true'
+  }else{
+    return 'false'
+  }
+}
