a86eebeff3218708f71d18e1fb42c5e8005dadc3
Nate Perdomo <101995665+perdomon@users.noreply.github.com>
2022-05-19 17:58:52 -0500

    Create simpleMultiplication.js

---
+++ 8kyu/simpleMultiplication.js
@@ -0,0 +1,10 @@
+// 8kyu Simple Multiplication
+
+// This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+
+
+
+function simpleMultiplication(number) {
+    solution = ((number % 2 == 0) ? number * 8 : number * 9)
+  return solution
+}
