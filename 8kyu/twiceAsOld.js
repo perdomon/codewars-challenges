b6c02833673f2c257d85ab1f7017930d335ce81e
Nate Perdomo <101995665+perdomon@users.noreply.github.com>
2022-05-19 17:57:54 -0500

    Update and rename NEXT-twiceAsOld to twiceAsOld.js

---
+++ 8kyu/twiceAsOld.js
@@ -0,0 +1,13 @@
+// 8kyu Twice As Old
+
+//Your function takes two arguments:
+
+// current father's age (years)
+// current age of his son (years)
+// Сalculate how many years ago the father was twice as old as his son (or in how many years he will be twice as old).
+
+function twiceAsOld(dadYearsOld, sonYearsOld) {
+  solution = Math.abs(dadYearsOld - sonYearsOld*2)
+  return solution
+}
+
