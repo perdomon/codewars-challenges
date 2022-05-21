4105cf7372dff200af158015444ac3fb1b753122
Nate Perdomo <101995665+perdomon@users.noreply.github.com>
2022-05-21 13:09:11 -0500

    Update NEXT-setAlarm.js

--- 8kyu/NEXT-setAlarm.js
+++ 8kyu/NEXT-setAlarm.js
@@ -1,4 +1,10 @@
 // 8kyu L1 - Set Alarm
 
-//
+// Write a function named setAlarm which receives two parameters. The first parameter, employed, is 
+// true whenever you are employed and the second parameter, vacation is true whenever you are on vacation.
 
+function setAlarm(employed, vacation){
+  return ((employed == true && vacation == false))
+
+  
+  
