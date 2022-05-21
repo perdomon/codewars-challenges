20d93780387d18a5515159517e248840b45ff17d
Nate Perdomo <101995665+perdomon@users.noreply.github.com>
2022-05-21 13:15:07 -0500

    Create booleanValueToString.js

---
+++ 8kyu/booleanValueToString.js
@@ -0,0 +1,9 @@
+// 8kyu Convert Boolean values to string values 'yes' and 'no'
+
+// Accept a boolean value and return the corresponding value as a string
+
+function boolToWord( bool ){
+  return(bool ? 'Yes' : 'No')
+}
+
+// Ternary operator condition ? ifTrue : ifFalse
