f4edda5ad19a3fe85785eb61d43a5cb67b647b1e
Nate Perdomo <101995665+perdomon@users.noreply.github.com>
2022-05-24 18:36:24 -0500

    Create noMap.js

---
+++ 8kyu/noMap.js
@@ -0,0 +1,8 @@
+// 8kyu Lost without a map
+
+// Given an array of integers, return a new array with each value doubled.
+
+function maps(x){
+  newArr = x.map(element => element * 2)
+  return newArr
+}
